d725ef9313eae68f9f344afae488209bda7d73bb
Uriel Enrique <urielenrique99@gmail.com>
2022-08-16 20:58:06 -0500

    Initial commit for HelloMVVM

---
+++ HelloMVVM/HelloMVVM/HelloMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloMVVMApp.swift
+//  HelloMVVM
+//
+//  Created by Uriel Hernandez Gonzalez on 16/08/22.
+//
+
+import SwiftUI
+
+@main
+struct HelloMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
