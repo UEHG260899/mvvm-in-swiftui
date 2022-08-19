dcd5512704e087cbfb2bd7a825af84e495dff571
Uriel Enrique <urielenrique99@gmail.com>
2022-08-18 20:52:31 -0500

    Basic structure for BankingApp

---
+++ BankApp/BankApp/BankAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankAppApp.swift
+//  BankApp
+//
+//  Created by Uriel Hernandez Gonzalez on 18/08/22.
+//
+
+import SwiftUI
+
+@main
+struct BankAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
