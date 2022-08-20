f183e247502c6f2928fe4ebfff52ee5ae48af64e
Uriel Enrique <urielenrique99@gmail.com>
2022-08-20 17:59:39 -0500

    Initial configs for Weather

---
+++ Weather/Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Uriel Hernandez Gonzalez on 20/08/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
